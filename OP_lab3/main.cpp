8f9282eb8b9ab3cd4d2b88a95731626a141fbd32
HoopeMax <nadezhda-maksimova-01@mail.ru>
2020-10-27 22:57:05 +0300

    другой вариант

--- OP_lab3/main.cpp
+++ OP_lab3/main.cpp
@@ -2,74 +2,94 @@
 
 class Figure
 {
-private:
-	int x;
-	int y;
+protected:
+
+	float* sideA;
+	float* sideB;
 
 public:
-	Figure()
-	{
-		x = 0;
-		y = 0;
-		std::cout << this << "конструктор" << std::endl;
-	}
 
-	Figure(int valX, int valY)
+	virtual float Perimetr(int size) = 0;
+	virtual void Area() = 0;
+	
+	Figure(int size)
 	{
-		x = valX;
-		y = valY;
-		std::cout << this << "конструктор" << std::endl;
+		this->sideA = new float[size];
+		this->sideB = new float[size];
 	}
-
-	void Print()
-	{
-		std::cout << x << std::endl;
-		std::cout << y << std::endl;
-	}
-
-	void SetY(int y)
-	{
-		this->y = y;
-	}
-
-	int GetY()
-	{
-		return y;
-	}
-
-	void SetX(int x)
-	{
-		this->x = x;
-	}
-
-	int GetX()
-	{
-		return x;
-	}
-
-	bool operator == (const Figure& newObject)//сам оператор перегрузки ==
-	{
-		return (this->x == newObject.x && this->y == newObject.y);
-	}
-
-	bool operator != (const Figure& newObject)// оператор перегрузки !=
-	{
-		return !(this->x == newObject.x && this->y == newObject.y);
-	}
+	
 };
+
+class Rectangle : public Figure
+{
+public:
+	Rectangle(int size, float* sideA) :Figure(size)
+	{
+		this->sideA = new float[size];
+
+		for (int i(0); i < size; i++)
+		{
+			std::cin >> sideA[i];
+			this->sideA[i] = sideA[i];
+			std::cin >> sideB[i];
+			this->sideB[i] = sideB[i];
+			std::cout << "\n" << this->sideA[i]<<"\n";
+			std::cout << "\n" << this->sideB[i] << "\n";
+		}
+
+	}
+	
+	float Perimetr(int size) override
+	{
+		float perimetr[2];
+		for (int i = 0; i < size; i++)
+		{
+			perimetr[i] = (sideA[i] + sideB[i]) * 2;
+			return perimetr[i];
+		}
+	}
+
+	void Area() override
+	{
+
+	}
+
+};
+
+//class Parallelogram : public Figure
+//{
+//public:
+//	Parallelogram(int size, float* sideA) :Figure(size)
+//	{
+//		
+//	}
+//	virtual void Perimetr() override
+//	{
+//
+//	}
+//	virtual void Area() override
+//	{
+//
+//	}
+//
+//};
 
 
 int main(void)
 {
-
+	
 	setlocale(LC_ALL, "rus");
 
-	Figure point(3, 5);
-	Figure point2(2, 5);
+	int N = 2, M = 3;
+	float* sideA;
+	sideA = new float[N];
+	
+	Rectangle object(N, sideA);
 
-	bool result1 = point == point2;//возвращает false
-	bool result2 = point != point2;//возвращает true(сравниваем точки из конструктора)
-
+	for (int i = 0; i < N; i++)
+	{
+		std::cout << object.Perimetr(N);
+	}
 
 	system("pause");
 	return 0;
