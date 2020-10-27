92bd0805e0d6a34d719bd7b30b5b1761153209df
HoopeMax <nadezhda-maksimova-01@mail.ru>
2020-10-28 02:39:40 +0300

    Добавила бесполезный ввлд с консоли)

--- OP_lab3/main.cpp
+++ OP_lab3/main.cpp
@@ -2,7 +2,6 @@
 #define _USE_MATH_DEFINES
 #include<math.h>
 
-
 class Figure
 {
 protected:
@@ -10,6 +9,7 @@ protected:
 	float sideA;
 	float sideB;
 
+	//чисто вирутальные функции
 	virtual float Perimetr() = 0;
 	virtual float Area() = 0;
 	virtual void Output() = 0;
@@ -25,7 +25,7 @@ public:
 class Rectangle : public Figure
 {
 public:
-	Rectangle(float sideA, float sideB) :Figure( sideA, sideB)
+	Rectangle(float sideA, float sideB) :Figure( sideA, sideB)//наследование конструктора
 	{
 		this->sideA = sideA;
 		this->sideB = sideB;
@@ -56,7 +56,7 @@ private:
 	float angle;
 public:
 	
-	float Perimetr() override
+	float Perimetr() override//переопределение методов 
 	{
 		return sideA + sideB + sideA + sideB;
 	}
@@ -67,12 +67,11 @@ public:
 	}
 
 	float Area() override
-	{
-		
+	{		
 		return sideB* Height();
 	}
 
-	Parallelogram(float sideA, float sideB, float angle) :Figure(sideA, sideB)
+	Parallelogram(float sideA, float sideB, float angle) :Figure(sideA, sideB)//наследование конструктора
 	{
 		this->angle = angle;
 		Perimetr();
@@ -87,43 +86,63 @@ public:
 	}
 
 };
+//Ввод сторон прямоугольника
+void Print_Side(float* side1, float* side2, unsigned short size);
 
+//Ввод сторон параллелограмма и угла (с перегрузкой)
+void Print_Side(float* side1, float* side2, unsigned short size, float* angle);
+
+//ищем минимальную площадь прямоугольников 
 float Find_Rect_Max(unsigned short size, Rectangle* object1);
+//ищем максимальную площадь прямоугольников 
 float Find_Rect_Min(unsigned short size, Rectangle* object1);
 
+//вывод информации о прмоугольнике 
 void Output_Rect(Rectangle* object1, unsigned short size);
+//вывод информации о параллелограмме
 void Output_Paral(Parallelogram* object2, unsigned short size);
-
+//ищем минимальную высоту параллелограмма
 float Find_Paral(unsigned short size, Parallelogram* object2);
 
 int main(void)
-{
-	
+{	
 	setlocale(LC_ALL, "rus");
 
-	const unsigned short N = 3, M = 2;
+	const unsigned short N = 4, M = 3;
 
-	/*float* sideA = new  float[N];
-	float* sideB = new  float[N];*/
+	float* rec_sideA = new  float[N];
+	float* rec_sideB = new  float[N];
 
-	/*for (unsigned short i(0); i < N; i++)
-	{
-		std::cin >> sideA[i];
-		std::cin >> sideB[i];
-	}*/
+	float* paral_sideA = new  float[M];
+	float* paral_sideB = new  float[M];
+	float* angle = new  float[M];
+
+	std::cout << std::endl << "Прямоугольники:" << std::endl << std::endl;
+	Print_Side(rec_sideA, rec_sideB, N);
+
+	std::cout << std::endl << "Пaраллелограммы:" << std::endl << std::endl;
+	Print_Side(paral_sideA, paral_sideB, M, angle);
 
 	Rectangle* object1 = new Rectangle[N]
 	{
-		
-		{2,5},
+		{rec_sideA[0], rec_sideB[0]},
+		{rec_sideA[1], rec_sideB[1]},
+		{rec_sideA[2], rec_sideB[2]},
+		{rec_sideA[3], rec_sideB[3]},
+	
+		/*{2,5},
 		{4,3},
-		{2,5}
+		{2,5}*/
 	};
 
 	Parallelogram* object2 = new Parallelogram[M]
-	{
-		{4,8,30},
-		{3,2,45}
+	{	
+		{paral_sideA[0], paral_sideB[0], angle[0]},
+		{paral_sideA[1], paral_sideB[1], angle[1]},
+		{paral_sideA[2], paral_sideB[2], angle[2]},
+		 
+		/*{4,8,30},
+		{3,2,45}*/
 	};
 
 	std::cout << std::endl << "Прямоугольники:" << std::endl << std::endl;
@@ -201,4 +220,29 @@ float Find_Paral(unsigned short size, Parallelogram* object2)
 		}
 	}
 	return min;
+}
+
+void Print_Side(float *side1, float* side2, unsigned short size)
+{
+	for (unsigned short i(0); i < size; i++)
+	{
+		
+		std::cout << "Введите сторону А : ";
+		std::cin >> side1[i];
+		std::cout << "Введите сторону B : ";
+		std::cin >> side2[i];
+	}
+}
+
+void Print_Side(float* side3, float* side4, unsigned short size, float* angle)
+{
+	for (unsigned short i(0); i < size; i++)
+	{
+		std::cout << "Введите сторону А : ";
+		std::cin >> side3[i];
+		std::cout << "Введите сторону B : ";
+		std::cin >> side4[i];
+		std::cout << "Введите угол : ";
+		std::cin >> angle[i];
+	}
 }
